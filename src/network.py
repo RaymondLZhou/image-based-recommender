2cc6268113445c92d1010cf10405eb18f31c001d
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-11 13:31:20 -0400

    Loaded images and preprocessed

--- src/network.py
+++ src/network.py
@@ -12,7 +12,7 @@ from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
 
 nb_closest_images = 5
-img_path = "../data/images/"
+imgs_path = "../data/images/"
 
 vgg_model = vgg16.VGG16(weights='imagenet')
 feat_extractor = Model(inputs=vgg_model.input, outputs=vgg_model.get_layer("fc2").output)
@@ -21,8 +21,22 @@ feat_extractor.summary()
 image_width = eval(str(vgg_model.layers[0].output.shape[1]))
 image_height = eval(str(vgg_model.layers[0].output.shape[2]))
 
-pil_img = load_img('../data/images/10000.jpg',  target_size=(image_width, image_height))
-array_img = img_to_array(pil_img)
-images = np.expand_dims(array_img, axis=0)
+files = [imgs_path + x for x in os.listdir(imgs_path) if "jpg" in x]
+print("number of images:", len(files))
 
+original = load_img(files[5],  target_size=(image_width, image_height))
+
+plt.imshow(original)
+plt.show()
+
+numpy_image = img_to_array(original)
+images = np.expand_dims(numpy_image, axis=0)
 print(images.shape)
+
+processed_image = preprocess_input(images.copy())
+
+img_features = feat_extractor.predict(processed_image)
+
+print("features successfully extracted!")
+print("number of image features:", img_features.size)
+print(img_features)
