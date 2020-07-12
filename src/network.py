7708dc5abc211d83e7ce532994bab36cb3a1f18f
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-12 11:40:51 -0400

    Processed layers

--- src/network.py
+++ src/network.py
@@ -1,70 +1,74 @@
 import tensorflow as tf
+import IPython.display as display
+
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12,12)
+mpl.rcParams['axes.grid'] = False
+
 import numpy as np
+import PIL.Image
 import time
+import functools
 
-from PIL import Image 
-
-from tensorflow.python.keras.preprocessing import image as kp_image
-from tensorflow.python.keras import models
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
 
 def load_img(path_to_img):
-    max_dim = 512
-    img = Image.open(path_to_img)
-    long = max(img.size)
-    scale = max_dim/long
-    img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), Image.ANTIALIAS)
-  
-    img = kp_image.img_to_array(img)
-  
-    img = np.expand_dims(img, axis=0)
-    return img
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
 
-def imshow(img, title=None):
-    out = np.squeeze(img, axis=0)
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
 
-    out = out.astype('uint8')
-    plt.imshow(out)
+  new_shape = tf.cast(shape * scale, tf.int32)
 
-    if title is not None:
-        plt.title(title)
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
 
-    plt.imshow(out)
+def imshow(image, title=None):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
 
-def load_and_process_img(path_to_img):
-    img = load_img(path_to_img)
-    img = tf.keras.applications.vgg19.preprocess_input(img)
-    return img
+  plt.imshow(image)
+  if title:
+    plt.title(title)
 
-def deprocess_img(processed_img):
-    x = processed_img.copy()
-    if len(x.shape) == 4:
-        x = np.squeeze(x, 0)
+plt.figure(figsize=(10,10)) 
 
-    assert len(x.shape) == 3, ("Input must be of dimension [1, height, width, channel] or [height, width, channel]")
-
-    if len(x.shape) != 3:
-        raise ValueError("Invalid input to deprocessing image")
-  
-    x[:, :, 0] += 103.939
-    x[:, :, 1] += 116.779
-    x[:, :, 2] += 123.68
-    x = x[:, :, ::-1]
-
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
-
-plt.figure(figsize=(10, 10))
-
-content = load_img('../data/content/turtle.jpg').astype('uint8')
-style = load_img('../data/style/Hokusai.jpg')
+content_image = load_img('../data/content/turtle.jpg')
+style_image = load_img('../data/style/Hokusai.jpg')
 
 plt.subplot(1, 2, 1)
-imshow(content, 'Content Image')
+imshow(content_image, 'Content Image')
 
 plt.subplot(1, 2, 2)
-imshow(style, 'Style Image')
+imshow(style_image, 'Style Image')
 plt.show()
+
+x = tf.keras.applications.vgg19.preprocess_input(content_image*255)
+x = tf.image.resize(x, (224, 224))
+vgg = tf.keras.applications.VGG19(include_top=True, weights='imagenet')
+prediction_probabilities = vgg(x)
+prediction_probabilities.shape
+
+predicted_top_5 = tf.keras.applications.vgg19.decode_predictions(prediction_probabilities.numpy())[0]
+[(class_name, prob) for (number, class_name, prob) in predicted_top_5]
+
+vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+
+print()
+for layer in vgg.layers:
+  print(layer.name)
 
 content_layers = ['block5_conv2'] 
 
@@ -72,36 +76,89 @@ style_layers = ['block1_conv1',
                 'block2_conv1',
                 'block3_conv1', 
                 'block4_conv1', 
-                'block5_conv1'
-               ]
+                'block5_conv1']
 
 num_content_layers = len(content_layers)
 num_style_layers = len(style_layers)
 
-def get_model():
-    vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
-    vgg.trainable = False
+def vgg_layers(layer_names):
+  """ Creates a vgg model that returns a list of intermediate output values."""
+  # Load our model. Load pretrained VGG, trained on imagenet data
+  vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+  vgg.trainable = False
+  
+  outputs = [vgg.get_layer(name).output for name in layer_names]
 
-    style_outputs = [vgg.get_layer(name).output for name in style_layers]
-    content_outputs = [vgg.get_layer(name).output for name in content_layers]
-    model_outputs = style_outputs + content_outputs
+  model = tf.keras.Model([vgg.input], outputs)
+  return model
 
-    return models.Model(vgg.input, model_outputs)
+style_extractor = vgg_layers(style_layers)
+style_outputs = style_extractor(style_image*255)
 
-def get_content_loss(base_content, target):
-    return tf.reduce_mean(tf.square(base_content - target))
+#Look at the statistics of each layer's output
+for name, output in zip(style_layers, style_outputs):
+  print(name)
+  print("  shape: ", output.numpy().shape)
+  print("  min: ", output.numpy().min())
+  print("  max: ", output.numpy().max())
+  print("  mean: ", output.numpy().mean())
+  print()
+
 
 def gram_matrix(input_tensor):
-    channels = int(input_tensor.shape[-1])
-    a = tf.reshape(input_tensor, [-1, channels])
-    n = tf.shape(a)[0]
-    gram = tf.matmul(a, a, transpose_a=True)
-    return gram / tf.cast(n, tf.float32)
+  result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+  input_shape = tf.shape(input_tensor)
+  num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+  return result/(num_locations)
 
-def get_style_loss(base_style, gram_target):
-    height, width, channels = base_style.get_shape().as_list()
-    gram_style = gram_matrix(base_style)
-  
-    return tf.reduce_mean(tf.square(gram_style - gram_target))
+class StyleContentModel(tf.keras.models.Model):
+  def __init__(self, style_layers, content_layers):
+    super(StyleContentModel, self).__init__()
+    self.vgg =  vgg_layers(style_layers + content_layers)
+    self.style_layers = style_layers
+    self.content_layers = content_layers
+    self.num_style_layers = len(style_layers)
+    self.vgg.trainable = False
 
+  def call(self, inputs):
+    "Expects float input in [0,1]"
+    inputs = inputs*255.0
+    preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+    outputs = self.vgg(preprocessed_input)
+    style_outputs, content_outputs = (outputs[:self.num_style_layers], 
+                                      outputs[self.num_style_layers:])
+
+    style_outputs = [gram_matrix(style_output)
+                     for style_output in style_outputs]
+
+    content_dict = {content_name:value 
+                    for content_name, value 
+                    in zip(self.content_layers, content_outputs)}
+
+    style_dict = {style_name:value
+                  for style_name, value
+                  in zip(self.style_layers, style_outputs)}
+    
+    return {'content':content_dict, 'style':style_dict}
+
+extractor = StyleContentModel(style_layers, content_layers)
+
+results = extractor(tf.constant(content_image))
+
+print('Styles:')
+for name, output in sorted(results['style'].items()):
+  print("  ", name)
+  print("    shape: ", output.numpy().shape)
+  print("    min: ", output.numpy().min())
+  print("    max: ", output.numpy().max())
+  print("    mean: ", output.numpy().mean())
+  print()
+
+print("Contents:")
+for name, output in sorted(results['content'].items()):
+  print("  ", name)
+  print("    shape: ", output.numpy().shape)
+  print("    min: ", output.numpy().min())
+  print("    max: ", output.numpy().max())
+  print("    mean: ", output.numpy().mean())
 
