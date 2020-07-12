c21caa1534e2b79d7539492f67238c8dd5bda496
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-12 11:06:35 -0400

    Processed data

---
+++ src/network.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+from PIL import Image 
+
+from tensorflow.python.keras.preprocessing import image as kp_image
+from tensorflow.python.keras import models
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = Image.open(path_to_img)
+    long = max(img.size)
+    scale = max_dim/long
+    img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), Image.ANTIALIAS)
+  
+    img = kp_image.img_to_array(img)
+  
+    img = np.expand_dims(img, axis=0)
+    return img
+
+def imshow(img, title=None):
+    out = np.squeeze(img, axis=0)
+
+    out = out.astype('uint8')
+    plt.imshow(out)
+
+    if title is not None:
+        plt.title(title)
+
+    plt.imshow(out)
+
+def load_and_process_img(path_to_img):
+    img = load_img(path_to_img)
+    img = tf.keras.applications.vgg19.preprocess_input(img)
+    return img
+
+def deprocess_img(processed_img):
+    x = processed_img.copy()
+    if len(x.shape) == 4:
+        x = np.squeeze(x, 0)
+
+    assert len(x.shape) == 3, ("Input must be of dimension [1, height, width, channel] or [height, width, channel]")
+
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+  
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
+
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+plt.figure(figsize=(10,10))
+
+content = load_img('../data/content/turtle.jpg').astype('uint8')
+style = load_img('../data/style/Hokusai.jpg')
+
+plt.subplot(1, 2, 1)
+imshow(content, 'Content Image')
+
+plt.subplot(1, 2, 2)
+imshow(style, 'Style Image')
+plt.show()
+
+content_layers = ['block5_conv2'] 
+
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1', 
+                'block4_conv1', 
+                'block5_conv1'
+               ]
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
