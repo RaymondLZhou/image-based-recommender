002d4441444e5e595449744e087bd46bc49566f2
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-12 09:12:04 -0400

    Added predictions

--- src/network.py
+++ src/network.py
@@ -22,7 +22,7 @@ image_width = eval(str(vgg_model.layers[0].output.shape[1]))
 image_height = eval(str(vgg_model.layers[0].output.shape[2]))
 
 files = [imgs_path + x for x in os.listdir(imgs_path) if "jpg" in x]
-files = files[:100]
+files = files[:500]
 print("Number of images:", len(files))
 
 importedImages = []
@@ -43,3 +43,30 @@ imgs_features = feat_extractor.predict(processed_imgs)
 print("Features successfully extracted!")
 print("Number of image features:", imgs_features.shape)
 print(imgs_features)
+
+cosSimilarities = cosine_similarity(imgs_features)
+cos_similarities_df = pd.DataFrame(cosSimilarities, columns=files, index=files)
+cos_similarities_df.head()
+
+def retrieve_most_similar_products(given_img):
+
+    print("-----------------------------------------------------------------------")
+    print("original product:")
+
+    original = load_img(given_img, target_size=(image_width, image_height))
+    plt.imshow(original)
+    plt.show()
+
+    print("-----------------------------------------------------------------------")
+    print("most similar products:")
+
+    closest_imgs = cos_similarities_df[given_img].sort_values(ascending=False)[1:nb_closest_images+1].index
+    closest_imgs_scores = cos_similarities_df[given_img].sort_values(ascending=False)[1:nb_closest_images+1]
+
+    for i in range(0, len(closest_imgs)):
+        original = load_img(closest_imgs[i], target_size=(image_width, image_height))
+        plt.imshow(original)
+        plt.show()
+        print("similarity score : ", closest_imgs_scores[i])
+
+retrieve_most_similar_products(files[1])
