695088d24393fd8fff187126c83344a88e0b3e76
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-13 19:44:44 -0400

    Updated naming convention of output

--- src/transfer.py
+++ src/transfer.py
@@ -144,17 +144,30 @@ def tensor_to_image(tensor):
     
     return PIL.Image.fromarray(tensor)
 
-content_path = '../data/content/turtle.jpg'
-style_path = '../data/style/Hokusai.jpg'
-output_path = '../data/output/stylized.jpg'
+content_layers = ['block5_conv2'] 
+style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+
+optimizer = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+style_weight = 1e-2
+content_weight = 1e4
+total_variation_weight = 30
+
+content_name = 'turtle'
+style_name = 'Hokusai'
+image_path = '../data/'
+
+epochs = 1
+steps_per_epoch = 2
+
+content_path = image_path + 'content/' + content_name + '.jpg'
+style_path = image_path + 'style/' + style_name + '.jpg'
+output_path = image_path + 'output/' + content_name + style_name + '.jpg'
 
 content_image = load_image(content_path)
 style_image = load_image(style_path)
 
 plot_images(content_image, style_image)
-
-content_layers = ['block5_conv2'] 
-style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
 
 num_content_layers = len(content_layers)
 num_style_layers = len(style_layers)
@@ -163,15 +176,6 @@ extractor = StyleContentModel(style_layers, content_layers)
 
 style_targets = extractor(style_image)['style']
 content_targets = extractor(content_image)['content']
-
-optimizer = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
-
-style_weight = 1e-2
-content_weight = 1e4
-total_variation_weight = 30
-
-epochs = 1
-steps_per_epoch = 2
 
 image = tf.Variable(content_image)
 
